78017732ff7ca38aa46bfabec734fad40126e9b9
Darwin Smith <darwinpsmith@gmail.com>
2019-05-04 13:17:23 -0400

    updates style Hero

--- js/app.js
+++ js/app.js
@@ -1,32 +1,43 @@
-$(document).ready(function () {
-    // Scroll the whole document
-    $.localScroll({
-        target: 'body',
-        duration: 220,
-        // Offeset for NavBar
-        offset: -72
-    });
+$(document).ready(function() {
+  // Scroll the whole document
+  $.localScroll({
+    target: "body",
+    duration: 220,
+    // Offeset for NavBar
+    offset: -72
+  });
 });
 
-const web =  document.querySelector('.hero-title');
-const navHeader =  document.querySelector('.nav-header');
-// const navLinks = document.querySelectorAll('.nav-item a');
-const words = ["Web", "Full Stack", "Front-End"]
+const web = document.querySelector(".hero-title");
+const navHeader = document.querySelector(".nav-header");
+const navLinks = document.querySelectorAll(".nav-item a, .logo");
+const hamburgerBtn = document.querySelector(".nav-toggle-label span");
+const words = ["Web", "Full Stack", "Front End"];
 
 const changeText = () => {
-    for (let i = 0; i < words.length; i++) {
-        web.textContent = words[Math.floor(Math.random() * 3)];
-        return;
-    }
-}
+  if (web.textContent === words[0]) {
+    web.textContent = words[1];
+  } else if (web.textContent === words[1]) {
+    web.textContent = words[2];
+  } else {
+    web.textContent = words[0];
+  }
+  return;
+  // }
+};
 
 const changeNavBg = () => {
-    if (window.scrollY > 10) {
-        navHeader.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
-    } else {
-        navHeader.style.backgroundColor = "rgb(246, 246, 246)";
-    }
-}
+  if (window.scrollY > 10) {
+    navHeader.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
+    navLinks.forEach(link => (link.style.color = "rgb(0, 0, 0)"));
+    hamburgerBtn.style.backgroundColor = "black";
+  } else {
+    navHeader.style.backgroundColor = "rgba(246, 246, 246, 0)";
+    navLinks.forEach(link => (link.style.color = "rgb(255, 255, 255)"));
+    hamburgerBtn.style.backgroundColor = "rgba(255, 255, 255, 1)";
+  }
+};
 
-window.addEventListener('scroll', changeNavBg)
-document.querySelector('.hero-text-container').addEventListener('mouseover', changeText);
+setInterval(changeText, 4000);
+
+window.addEventListener("scroll", changeNavBg);
