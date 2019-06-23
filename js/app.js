1ab998991ec376937d237c2332b2b7e261aa70f3
Darwin Smith <darwinpsmith@gmail.com>
2019-06-23 13:08:15 -0400

    added interesction observers for about section and projects

--- js/app.js
+++ js/app.js
@@ -8,22 +8,21 @@ $(document).ready(function() {
   });
 });
 
-const web = document.querySelector(".hero-title");
+const title = document.querySelector(".hero-title");
 const navHeader = document.querySelector(".nav-header");
 const navLinks = document.querySelectorAll(".nav-item a, .logo");
 const hamburgerBtn = document.querySelector(".nav-toggle-label span");
 const words = ["Web", "Full Stack", "Front End"];
 
 const changeText = () => {
-  if (web.textContent === words[0]) {
-    web.textContent = words[1];
-  } else if (web.textContent === words[1]) {
-    web.textContent = words[2];
+  if (title.textContent === words[0]) {
+    title.textContent = words[1];
+  } else if (title.textContent === words[1]) {
+    title.textContent = words[2];
   } else {
-    web.textContent = words[0];
+    title.textContent = words[0];
   }
   return;
-  // }
 };
 
 const changeNavBg = () => {
@@ -41,3 +40,40 @@ const changeNavBg = () => {
 setInterval(changeText, 4000);
 
 window.addEventListener("scroll", changeNavBg);
+
+const projects = document.querySelectorAll(".row");
+
+const observerOptions = {
+  root: null,
+  threshold: 0.25,
+  rootMargin: "0px"
+};
+
+const projectsObserver = new IntersectionObserver(
+  (entries, projectsObserver) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // console.log("intersecting");
+        entry.target.classList.add("in-view");
+      }
+    });
+  },
+  observerOptions
+);
+
+const sectionObserver = new IntersectionObserver((entries, sectionObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log("Intersecting");
+      entry.target.classList.add("in-view");
+    }
+  });
+}, observerOptions);
+
+const aboutSection = document.querySelector(".about");
+
+sectionObserver.observe(aboutSection);
+
+projects.forEach(project => {
+  projectsObserver.observe(project);
+});
